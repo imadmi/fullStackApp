4c87f7075b84958659e461a66cbe4b03ddb8cdf0
imad <imad.mimouni.123@gmail.com>
2023-11-01 13:02:13 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const Note = (props) => {
-  return <li>{props.note.content}</li>;
+
+  // const toggleImportanceOf = id => {
+  //   const url = `http://localhost:3001/notes/${id}`
+  //   const note = notes.find(n => n.id === id)
+  //   const changedNote = { ...note, important: !note.important }
+  
+  //   axios.put(url, changedNote).then(response => {
+  //     setNotes(notes.map(n => n.id !== id ? n : response.data))
+  //   })
+  // }
+  
+  return (
+    <>
+      <li>{props.note.content}</li>
+      {/* <button onClick={toggleImportanceOf({props.note.id})}> impo </button> */}
+    </>
+  );
 };
 
 const App = () => {
@@ -21,7 +37,6 @@ const App = () => {
 
   console.log("render", notes.length, "notes");
 
-
   const addNote = event => {
     event.preventDefault()
     const noteObject = {
@@ -33,6 +48,7 @@ const App = () => {
       .post('http://localhost:3001/notes', noteObject)
       .then(response => {
         console.log(response)
+        setNotes(notes.concat(response.data))
       })
   }
 
@@ -40,19 +56,31 @@ const App = () => {
     console.log(event.target.value);
     setNewNote(event.target.value);
   };
-
+  
+  const putrec = () => {  
+    axios.put("http://localhost:3001/notes/5", {
+      content: "edited using put requist",
+      important: true,
+    }).then(response => {
+      console.log(response)
+    }).catch(err => {
+      console.log(err)
+    })
+  } 
+  
   return (
     <div>
       <h1>Notes</h1>
       <ul>
         {notes.map((note) => (
           <Note key={note.id} note={note} />
-        ))}
+          ))}
       </ul>
       <form onSubmit={addNote}>
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
+      <button onClick={putrec}> putrec</button>
     </div>
   );
 };
