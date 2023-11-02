a996cf2cfa3310da04451ae85ac4f4f1765d0b07
imad <imad.mimouni.123@gmail.com>
2023-11-02 18:41:00 +0100

    adding middleware

--- server/index.js
+++ server/index.js
@@ -3,6 +3,19 @@
 const express = require("express");
 const app = express();
 
+// Middleware : function that can be used for handling request and response objects
+const requestLogger = (request, response, next) => {
+    console.log("Method:", request.method);
+    console.log("Path:  ", request.path);
+    console.log("Body:  ", request.body);
+    console.log("---");
+    next();
+};
+
+app.use(requestLogger);
+app.use(express.json());
+
+
 let notes = [
   {
     id: 1,
@@ -43,12 +56,37 @@ app.get("/notes/:id", (request, response) => {
   }
 });
 
-app.delete('/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-  
-    response.status(204).end()
-  })
+app.delete("/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
+});
 
 const PORT = 3001;
 app.listen(PORT, () => {
